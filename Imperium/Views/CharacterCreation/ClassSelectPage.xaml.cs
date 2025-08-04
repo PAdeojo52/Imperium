23ad2f37b39ad0983f1bc72712c8a2f346ee115d
Peter Adeojo <padeojo27@gmail.com>
2025-08-04 14:11:48 -0500

    Updated CSP with dummy data

--- Imperium/Views/CharacterCreation/ClassSelectPage.xaml.cs
+++ Imperium/Views/CharacterCreation/ClassSelectPage.xaml.cs
@@ -20,9 +20,68 @@ namespace Imperium.Views.CharacterCreation
     /// </summary>
     public partial class ClassSelectPage : Page
     {
+        // test data supplier
+        private Dictionary<string, List<string>> _itemsByTab;
         public ClassSelectPage()
         {
             InitializeComponent();
+            BuildTestData();
+            // populate initial tab
+            PopulateForTab("Race");
         }
+
+        private void BuildTestData()
+        {
+            _itemsByTab = new Dictionary<string, List<string>>(StringComparer.OrdinalIgnoreCase)
+            {
+                ["Race"] = new List<string> { "TestRace1", "TestRace2", "TestRace3" },
+                ["Background"] = new List<string> { "TestBackground1", "TestBackground2", "TestBackground3" },
+                ["Class"] = new List<string> { "TestClass1", "TestClass2", "TestClass3" },
+                ["Attributes"] = new List<string> { "TestAttribute1", "TestAttribute2", "TestAttribute3" },
+                ["Addictions"] = new List<string> { "TestAddiction1", "TestAddiction2", "TestAddiction3" }
+            };
+        }
+
+        private void AttributeTabs_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (AttributeTabs.SelectedItem is TabItem tab)
+            {
+                PopulateForTab(tab.Header.ToString());
+            }
+        }
+
+        private void PopulateForTab(string tabName)
+        {
+            // 1. ComboBox items
+            if (_itemsByTab.TryGetValue(tabName, out var list))
+                AvailableAttributesComboBox.ItemsSource = list;
+            else
+                AvailableAttributesComboBox.ItemsSource = null;
+
+            // 2. Button text
+            AddAttributeButton.Content = $"Add This {tabName.TrimEnd('s')}";
+
+            // 3. Clear previous selection/description
+
+            // 3. Select first entry so dropdown starts on it
+            if (AvailableAttributesComboBox.Items.Count > 0)
+                AvailableAttributesComboBox.SelectedIndex = 0;
+        }
+
+
+        private void AvailableAttributesComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (AvailableAttributesComboBox.SelectedItem is string name)
+            {
+                // Example description
+                AttributeDescriptionTextBox.Text = $"Description Of {name}";
+            }
+            else
+            {
+                AttributeDescriptionTextBox.Clear();
+            }
+        }
+
+
     }
 }
